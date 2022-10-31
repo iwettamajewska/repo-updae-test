86a2a792d1ed78c38d54759a694b1ce5ce2b9b24
iwetta <iwetta.majewska@gmail.com>
2022-10-31 11:03:35 +0100

    my-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FirstExample from "./comonents/FirstExample";
+import SecondExample from "./comonents/SecondExample";
+
+function App() {
+  return (
+    <>
+      <FirstExample />
+      <SecondExample />
+    </>
+  );
+}
+
+export default App;
